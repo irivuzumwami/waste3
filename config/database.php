60ffaafcc2bf4bda03ba57c91268179003eb2dbc
irivuzumwami <irivuzumwamijeandedieu2@gmail.com>
2026-05-11 14:14:20 +0200

    new dbconfig

--- config/database.php
+++ config/database.php
@@ -7,7 +7,7 @@ $password = 'npg_t1oWv2cmpDFN';
 $port = '5432';
 
 try {
-    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $username, $password);
+    $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
     die("Connection failed: " . $e->getMessage());
